3835ed177135abc3d171c903b20b5f76128e3cbd
Cap-Naomi <122072430+Cap-Naomi@users.noreply.github.com>
2026-04-21 16:53:20 -0500

    update simulator.cpp
    Refactor CPU structure to include MIPS_CPU, Control_Unit, and separate ALU and Memory classes.

--- simulator.cpp
+++ simulator.cpp
@@ -1,67 +1,79 @@
 #include <cstdint>
 #include <string>
 #include <map>
+#include "mips_simulator.hpp"
 
-/* Questions: 
-- which emitted control signals should be shown (RegWrite, MemWrite, MemRead, ALUOp...)
-- State registers: program counter, stack pointer, instruction register ? any others
-
+/* Notes: 
+- State registers: show state at point between stages in pipeline (pipeline latches)
 */
 
 
-class Registers{
-    registerFile[32];
-    unordered_map<string, int> registerMap{
-    {"$zero", 0},
-    {"$at", 1},
-    {"$v0", 2},
-    {"$v1", 3},
-    {"$a0", 4},
-    {"$a1", 5},
-    {"$a2", 6},
-    {"$a3", 7},
-    {"$t0", 8},
-    {"$t1", 9},
-    {"$t2", 10},
-    {"$t3", 11},
-    {"$t4", 12},
-    {"$t5", 13},
-    {"$t6", 14},
-    {"$t7", 15},
-    {"$s0", 16},
-    {"$s1", 17},
-    {"$s2", 18},
-    {"$s3", 19},
-    {"$s4", 20},
-    {"$s4", 21},
-    {"$s6", 22},
-    {"$s7", 23},
-    {"$t8", 24},
-    {"$t9", 25},
-    {"$k0", 26},
-    {"$k1", 27},
-    {"$gp", 28},
-    {"$sp", 29},
-    {"$fp", 30},
-    {"$ra", 31}, 
-    }
-};
-
-class ControlSignals{
-
+class Registers : public MIPS_CPU {
+    public:
+        int registerFile[32];
+        unordered_map<string, int> regMap{
+        {"$zero", 0},
+        {"$at", 1},
+        {"$v0", 2},
+        {"$v1", 3},
+        {"$a0", 4},
+        {"$a1", 5},
+        {"$a2", 6},
+        {"$a3", 7},
+        {"$t0", 8},
+        {"$t1", 9},
+        {"$t2", 10},
+        {"$t3", 11},
+        {"$t4", 12},
+        {"$t5", 13},
+        {"$t6", 14},
+        {"$t7", 15},
+        {"$s0", 16},
+        {"$s1", 17},
+        {"$s2", 18},
+        {"$s3", 19},
+        {"$s4", 20},
+        {"$s4", 21},
+        {"$s6", 22},
+        {"$s7", 23},
+        {"$t8", 24},
+        {"$t9", 25},
+        {"$k0", 26},
+        {"$k1", 27},
+        {"$gp", 28},
+        {"$sp", 29},
+        {"$fp", 30},
+        {"$ra", 31}, 
+        }
 
 };
 
+class Control_Unit{ // allows simulation of 5-stage pipeline
+// Control unit signals:
+    bool RegDest = false;
+    bool ALUOp = false;
+    bool ALUSrc = false;
+    bool MemRead = false;
+    bool MemWrite = false;
+    bool MemtoReg = false;
+    bool RegWrite = false;
+    bool Branch = false; // jump / beq 
 
-class CPU{
-    Registers registers;
-    int memory[1024]; // for sw and lw ?
-    int PC = 0; // need for beq / jump --- but dont really know why
-    string IR; // instruction register - current instruction
-    bool debugMode = false;
+    void fetch(){} // fetch instruction, update pc
 
-    void ADD(string rDest, string r1, string r2){
-        registers
+    void decode(){} // decode instruction, read from register file
+
+    void execute(){} // execute arithmetic instruction, calculate mem addr
+
+    void memory(){} // read/write data from/to memory
+
+    void write_back(){} // write data back to regFile
+};
+
+class CPU_ALU{ // handles instructions 
+
+    void ADD(string rDest, string r1, string r2){ // scope ???
+        register.registerFile[regMap[rDest]] = register.registerFile[regMap[r1]] + register.registerFile[regMap[rDest]];
     }
 
     void ADDI(string rDest, string r1, int num){}
@@ -78,14 +90,37 @@ class CPU{
 
     void SLR(string rDest, string r1, int num){}
 
-    void LW(string rDest, int offset, int r1){}
-
-    void SW(string int r1, int offest, string rDest){}
-
+    // BRANCH Intructions 
     void BEQ(string rDest, string r1, string label){}
 
     void JUMP(string label){}
 
+    // do nothing 
     void NOP(){}
+};
 
+class CPU_Memory{ // holds cpu memory and handles lw and sw instructions 
+    int data_memory[1024]; // for sw and lw
+
+    void LW(string rDest, int offset, int r1){}
+
+    void SW(string r1, int offest, string rDest){}
+};
+
+class MIPS_CPU{
+    public:
+        Registers registers;
+        CPU_ALU ALU;
+        CPU_Memory Memory;
+        Control_Unit control_unit;
+        int PC; // need for beq / jump
+        string IR; // instruction register - current instruction
+        bool debugMode;
+
+        MIPS_CPU() {
+            int PC = 0;
+            string IR = "";
+            bool debugMode = false;
+        }
+    
 };
