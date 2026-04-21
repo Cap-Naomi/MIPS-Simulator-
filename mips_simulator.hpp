50fc8e929e2370b1d6420e776ff6d99f84460b4f
Cap-Naomi <122072430+Cap-Naomi@users.noreply.github.com>
2026-04-21 16:54:28 -0500

    update mips_simulator.hpp
    Refactored CPU and Registers classes, added Control Unit and Memory classes.

--- mips_simulator.hpp
+++ mips_simulator.hpp
@@ -1,19 +1,39 @@
 #include <cstdint>
 #include <string>
-#include <map>
+#include <unordered_map>
+using namespace std;
+
 
 class Registers{
-    registerFile[32]; // holds 32 register
-    unordered_map<string, int> registerMap; // maps each register to index of registerFile
+    public:
+        int registerFile[32]; // holds 32 register
+        unordered_map<string, int> regMap; // maps each register to index of registerFile
 };
 
+class Control_Unit{
+// Control unit signals:
+    bool RegDest = false;
+    bool ALUOp = false;
+    bool ALUSrc = false;
+    bool MemRead = false;
+    bool MemWrite = false;
+    bool MemtoReg = false;
+    bool RegWrite = false;
+    bool Branch = false; // jump / beq
 
-class CPU{
-    Registers registers;
-    int memory[1024]; // dont know what this will do yet
-    int PC = 0; // need for beq / jump --- but dont really know why 
+    void fetch(); // fetch instruction, update pc
 
+    void decode(); // decode instruction, read from register file
 
+    void execute(); // execute arithmetic instruction, calculate mem addr
+
+    void memory(); // read/write data from/to memory
+
+    void write_back(); // write data back to regFile
+};
+
+class CPU_ALU
+{
     void ADD(string rDest, string r1, string r2);
     void ADDI(string rDest, string r1, int num);
     void SUB(string rDest, string r1, string r2);
@@ -22,9 +42,27 @@ class CPU{
     void OR(string rDest, string r1, string r2);
     void SLL(string rDest, string r1, int num);
     void SLR(string rDest, string r1, int num);
-    void LW(string rDest, int offset, int r1);
-    void SW(string int r1, int offest, string rDest);
     void BEQ(string rDest, string r1, string label);
     void JUMP(string label);
     void NOP();
+};
+
+class CPU_Memory
+{
+    int data_memory[1024];
+    void LW(string rDest, int offset, int r1);
+    void SW(string r1, int offest, string rDest);
+};
+
+class MIPS_CPU
+{
+    Registers registers;
+    CPU_ALU ALU;
+    CPU_Memory Memory;
+    Control_Unit control_unit;
+    int PC;
+    string IR;
+    bool debugMode;
+
+    MIPS_CPU();
 };
