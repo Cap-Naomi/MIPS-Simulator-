9256083b2da695ccbb0f0f02700eb98e9b4518d6
Naomi <naomi.capdeville@gmail.com>
2026-04-22 01:40:18 -0500

    Update structure of mips_simulator.hpp and simulator.cpp

--- mips_simulator.hpp
+++ mips_simulator.hpp
@@ -4,65 +4,109 @@
 using namespace std;
 
 
-class Registers{
+class RegisterFile{
     public:
-        int registerFile[32]; // holds 32 register
-        unordered_map<string, int> regMap; // maps each register to index of registerFile
+        int registers[32]; // holds 32 register
+        unordered_map<string, int> regMap{ // maps each register to index of registerFile
+            {"$zero", 0},
+            {"$at", 1},
+            {"$v0", 2},
+            {"$v1", 3},
+            {"$a0", 4},
+            {"$a1", 5},
+            {"$a2", 6},
+            {"$a3", 7},
+            {"$t0", 8},
+            {"$t1", 9},
+            {"$t2", 10},
+            {"$t3", 11},
+            {"$t4", 12},
+            {"$t5", 13},
+            {"$t6", 14},
+            {"$t7", 15},
+            {"$s0", 16},
+            {"$s1", 17},
+            {"$s2", 18},
+            {"$s3", 19},
+            {"$s4", 20},
+            {"$s5", 21},
+            {"$s6", 22},
+            {"$s7", 23},
+            {"$t8", 24},
+            {"$t9", 25},
+            {"$k0", 26},
+            {"$k1", 27},
+            {"$gp", 28},
+            {"$sp", 29},
+            {"$fp", 30},
+            {"$ra", 31}};
+
+        int get_val();
+        void set_val();
 };
 
 class Control_Unit{
-// Control unit signals:
-    bool RegDest = false;
-    bool ALUOp = false;
-    bool ALUSrc = false;
-    bool MemRead = false;
-    bool MemWrite = false;
-    bool MemtoReg = false;
-    bool RegWrite = false;
-    bool Branch = false; // jump / beq
+    public:
+    // Control unit signals:
+        bool RegDest = false;
+        bool ALUOp = false;
+        bool ALUSrc = false;
+        bool MemRead = false;
+        bool MemWrite = false;
+        bool MemtoReg = false;
+        bool RegWrite = false;
+        bool Branch = false; // jump / beq
 
-    void fetch(); // fetch instruction, update pc
+        void fetch(); // fetch instruction, update pc
 
-    void decode(); // decode instruction, read from register file
+        void decode(); // decode instruction, read from register file
 
-    void execute(); // execute arithmetic instruction, calculate mem addr
+        void execute(); // execute arithmetic instruction, calculate mem addr
 
-    void memory(); // read/write data from/to memory
+        void memory(); // read/write data from/to memory
 
-    void write_back(); // write data back to regFile
+        void write_back(); // write data back to regFile
 };
 
 class CPU_ALU
 {
-    void ADD(string rDest, string r1, string r2);
-    void ADDI(string rDest, string r1, int num);
-    void SUB(string rDest, string r1, string r2);
-    void MUL(string rDest, string r1, string r2);
-    void AND(string rDest, string r1, string r2);
-    void OR(string rDest, string r1, string r2);
-    void SLL(string rDest, string r1, int num);
-    void SLR(string rDest, string r1, int num);
-    void BEQ(string rDest, string r1, string label);
-    void JUMP(string label);
-    void NOP();
+    private:
+        RegisterFile &regFile;
+
+    public:
+        CPU_ALU(RegisterFile &regFile);
+
+        void ADD(string rDest, string r1, string r2);
+        void ADDI(string rDest, string r1, int num);
+        void SUB(string rDest, string r1, string r2);
+        void MUL(string rDest, string r1, string r2);
+        void AND(string rDest, string r1, string r2);
+        void OR(string rDest, string r1, string r2);
+        void SLL(string rDest, string r1, int num);
+        void SLR(string rDest, string r1, int num);
+        void BEQ(string rDest, string r1, string label);
+        void JUMP(string label);
+        void NOP();
 };
 
 class CPU_Memory
 {
-    int data_memory[1024];
-    void LW(string rDest, int offset, int r1);
-    void SW(string r1, int offest, string rDest);
+    public:
+        int data_memory[1024];
+        void LW(string rDest, int offset, int r1);
+        void SW(string r1, int offest, string rDest);
 };
 
-class MIPS_CPU
+class CPU
 {
-    Registers registers;
-    CPU_ALU ALU;
-    CPU_Memory Memory;
-    Control_Unit control_unit;
-    int PC;
-    string IR;
-    bool debugMode;
+    public:
+        RegisterFile regFile;
+        CPU_ALU ALU;
+        CPU_Memory Memory;
+        Control_Unit control_unit;
+        int PC;
+        string IR;
+        bool debugMode;
 
-    MIPS_CPU();
+        CPU();
 };
--- simulator.cpp
+++ simulator.cpp
@@ -1,126 +1,83 @@
 #include <cstdint>
 #include <string>
-#include <map>
+#include <unordered_map>
 #include "mips_simulator.hpp"
+#include <iostream>
+using namespace std;
 
 /* Notes: 
 - State registers: show state at point between stages in pipeline (pipeline latches)
 */
 
 
-class Registers : public MIPS_CPU {
-    public:
-        int registerFile[32];
-        unordered_map<string, int> regMap{
-        {"$zero", 0},
-        {"$at", 1},
-        {"$v0", 2},
-        {"$v1", 3},
-        {"$a0", 4},
-        {"$a1", 5},
-        {"$a2", 6},
-        {"$a3", 7},
-        {"$t0", 8},
-        {"$t1", 9},
-        {"$t2", 10},
-        {"$t3", 11},
-        {"$t4", 12},
-        {"$t5", 13},
-        {"$t6", 14},
-        {"$t7", 15},
-        {"$s0", 16},
-        {"$s1", 17},
-        {"$s2", 18},
-        {"$s3", 19},
-        {"$s4", 20},
-        {"$s4", 21},
-        {"$s6", 22},
-        {"$s7", 23},
-        {"$t8", 24},
-        {"$t9", 25},
-        {"$k0", 26},
-        {"$k1", 27},
-        {"$gp", 28},
-        {"$sp", 29},
-        {"$fp", 30},
-        {"$ra", 31}, 
-        }
+void CPU_ALU :: ADD(string rDest, string r1, string r2){
+    regFile.registers[regMap[rDest]] = (
+    regFile.registers[regMap[r1]] + regFile.registers[regMap[r2]])
+}
 
-};
 
-class Control_Unit{ // allows simulation of 5-stage pipeline
-// Control unit signals:
-    bool RegDest = false;
-    bool ALUOp = false;
-    bool ALUSrc = false;
-    bool MemRead = false;
-    bool MemWrite = false;
-    bool MemtoReg = false;
-    bool RegWrite = false;
-    bool Branch = false; // jump / beq 
+// --- CPU --- //
 
-    void fetch(){} // fetch instruction, update pc
+CPU :: CPU(){
+    int PC = 0;
+    string IR = "";
+    bool debugMode = false;
+}
 
-    void decode(){} // decode instruction, read from register file
 
-    void execute(){} // execute arithmetic instruction, calculate mem addr
+// --- REGISTER FILE --- //
 
-    void memory(){} // read/write data from/to memory
+int RegisterFile :: get_val(string regName){
+    return registers[regMap["regName"]];
+}
 
-    void write_back(){} // write data back to regFile
-};
+void RegisterFile :: set_val(string regName, int val){
+    registers[regMap["regName"]] = val;
+}
 
-class CPU_ALU{ // handles instructions 
 
-    void ADD(string rDest, string r1, string r2){ // scope ???
-        register.registerFile[regMap[rDest]] = register.registerFile[regMap[r1]] + register.registerFile[regMap[rDest]];
-    }
+// --- ALU --- //
 
-    void ADDI(string rDest, string r1, int num){}
+//     void ADD(string rDest, string r1, string r2) { cout << "add"; }
 
-    void SUB(string rDest, string r1, string r2){}
+//     void ADDI(string rDest, string r1, int num){ cout << "addi"; }
 
-    void MUL(string rDest, string r1, string r2){}
+//     void SUB(string rDest, string r1, string r2){}
 
-    void AND(string rDest, string r1, string r2){}
+//     void MUL(string rDest, string r1, string r2){}
 
-    void OR(string rDest, string r1, string r2){}
+//     void AND(string rDest, string r1, string r2){}
 
-    void SLL(string rDest, string r1, int num){}
+//     void OR(string rDest, string r1, string r2){}
 
-    void SLR(string rDest, string r1, int num){}
+//     void SLL(string rDest, string r1, int num){}
 
-    // BRANCH Intructions 
-    void BEQ(string rDest, string r1, string label){}
+//     void SLR(string rDest, string r1, int num){}
 
-    void JUMP(string label){}
+//     // BRANCH Intructions 
+//     void BEQ(string rDest, string r1, string label){}
 
-    // do nothing 
-    void NOP(){}
-};
+//     void JUMP(string label){}
 
-class CPU_Memory{ // holds cpu memory and handles lw and sw instructions 
-    int data_memory[1024]; // for sw and lw
+//     // do nothing 
+//     void NOP(){}
 
-    void LW(string rDest, int offset, int r1){}
 
-    void SW(string r1, int offest, string rDest){}
-};
+// --- CONTROL UNIT --- //
 
-class MIPS_CPU{
-    public:
-        Registers registers;
-        CPU_ALU ALU;
-        CPU_Memory Memory;
-        Control_Unit control_unit;
-        int PC; // need for beq / jump
-        string IR; // instruction register - current instruction
-        bool debugMode;
+// void fetch(){} // fetch instruction, update pc
 
-        MIPS_CPU() {
-            int PC = 0;
-            string IR = "";
-            bool debugMode = false;
-        }
-    
-};
+// void decode(){} // decode instruction, read from register file
+
+// void execute(){} // execute arithmetic instruction, calculate mem addr
+
+// void memory(){} // read/write data from/to memory
+
+// void write_back(){} // write data back to regFile
+
+
+// --- MEMORY --- //
+
+//     void LW(string rDest, int offset, int r1){}
+
+//     void SW(string r1, int offest, string rDest){}
